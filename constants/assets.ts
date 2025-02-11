8f135bb10de63639504ecce971b9524249139e50
zelan <zelan.xilin@outlook.com>
2025-02-11 18:52:28 +0800

    添加多个图标和插图文件

---
+++ constants/assets.ts
@@ -0,0 +1,175 @@
+import card1 from "../assets/card/card-1.png";
+import card2 from "../assets/card/card-2.png";
+export const card = {
+  card1,
+  card2,
+};
+
+import contacts from "../assets/category/contacts.png";
+import creditCardIn from "../assets/category/credit-card-in.png";
+import creditCard from "../assets/category/credit-card.png";
+import fileParagraph from "../assets/category/file-paragraph.png";
+import mobileBanking from "../assets/category/mobile-banking.png";
+import pig from "../assets/category/pig.png";
+import receipt from "../assets/category/receipt.png";
+import syncDevices from "../assets/category/sync-devices.png";
+import wallet from "../assets/category/wallet.png";
+export const category = {
+  contacts,
+  creditCardIn,
+  creditCard,
+  fileParagraph,
+  mobileBanking,
+  pig,
+  receipt,
+  syncDevices,
+  wallet,
+};
+
+import AR from "../assets/country/AR.png";
+import AT from "../assets/country/AT.png";
+import CN from "../assets/country/CN.png";
+import EN from "../assets/country/EN.png";
+import FR from "../assets/country/FR.png";
+import IN from "../assets/country/IN.png";
+import JP from "../assets/country/JP.png";
+import KR from "../assets/country/KR.png";
+import NI from "../assets/country/NI.png";
+import PT from "../assets/country/PT.png";
+import RU from "../assets/country/RU.png";
+import UA from "../assets/country/UA.png";
+import VN from "../assets/country/VN.png";
+export const country = {
+  AR,
+  AT,
+  CN,
+  EN,
+  FR,
+  IN,
+  JP,
+  KR,
+  NI,
+  PT,
+  RU,
+  UA,
+  VN,
+};
+
+import icon01 from "../assets/icon/01.png";
+import icon02 from "../assets/icon/02.png";
+import icon03 from "../assets/icon/03.png";
+import icon04 from "../assets/icon/04.png";
+import icon05 from "../assets/icon/05.png";
+import icon06 from "../assets/icon/06.png";
+import icon07 from "../assets/icon/07.png";
+import icon08 from "../assets/icon/08.png";
+import icon09 from "../assets/icon/09.png";
+import icon10 from "../assets/icon/10.png";
+import icon11 from "../assets/icon/11.png";
+import icon12 from "../assets/icon/12.png";
+import icon13 from "../assets/icon/13.png";
+import icon14 from "../assets/icon/14.png";
+import icon15 from "../assets/icon/15.png";
+import icon16 from "../assets/icon/16.png";
+import icon17 from "../assets/icon/17.png";
+import icon18 from "../assets/icon/18.png";
+import icon19 from "../assets/icon/19.png";
+import icon20 from "../assets/icon/20.png";
+import icon21 from "../assets/icon/21.png";
+import icon22 from "../assets/icon/22.png";
+import icon23 from "../assets/icon/23.png";
+import icon24 from "../assets/icon/24.png";
+import icon25 from "../assets/icon/25.png";
+import icon26 from "../assets/icon/26.png";
+import icon27 from "../assets/icon/27.png";
+import icon28 from "../assets/icon/28.png";
+import icon29 from "../assets/icon/29.png";
+import icon30 from "../assets/icon/30.png";
+import icon31 from "../assets/icon/31.png";
+import icon32 from "../assets/icon/32.png";
+import icon33 from "../assets/icon/33.png";
+import icon34 from "../assets/icon/34.png";
+import icon35 from "../assets/icon/35.png";
+import icon36 from "../assets/icon/36.png";
+import icon37 from "../assets/icon/37.png";
+import icon38 from "../assets/icon/38.png";
+import icon39 from "../assets/icon/39.png";
+import icon40 from "../assets/icon/40.png";
+import icon41 from "../assets/icon/41.png";
+import icon42 from "../assets/icon/42.png";
+import icon43 from "../assets/icon/43.png";
+import icon44 from "../assets/icon/44.png";
+import icon45 from "../assets/icon/45.png";
+import icon46 from "../assets/icon/46.png";
+import icon47 from "../assets/icon/47.png";
+export const icon = {
+  icon01,
+  icon02,
+  icon03,
+  icon04,
+  icon05,
+  icon06,
+  icon07,
+  icon08,
+  icon09,
+  icon10,
+  icon11,
+  icon12,
+  icon13,
+  icon14,
+  icon15,
+  icon16,
+  icon17,
+  icon18,
+  icon19,
+  icon20,
+  icon21,
+  icon22,
+  icon23,
+  icon24,
+  icon25,
+  icon26,
+  icon27,
+  icon28,
+  icon29,
+  icon30,
+  icon31,
+  icon32,
+  icon33,
+  icon34,
+  icon35,
+  icon36,
+  icon37,
+  icon38,
+  icon39,
+  icon40,
+  icon41,
+  icon42,
+  icon43,
+  icon44,
+  icon45,
+  icon46,
+  icon47,
+};
+
+import illustration1 from "../assets/illustration/Illustration-1.png";
+import illustration2 from "../assets/illustration/Illustration-2.png";
+import illustration3 from "../assets/illustration/Illustration-3.png";
+import illustration4 from "../assets/illustration/Illustration-4.png";
+import illustration5 from "../assets/illustration/Illustration-5.png";
+export const illustration = {
+  illustration1,
+  illustration2,
+  illustration3,
+  illustration4,
+  illustration5,
+};
+
+import faceId from "../assets/setting/face-id.png";
+import fingerprint from "../assets/setting/fingerprint.png";
+import safe from "../assets/setting/safe.png";
+export const setting = {
+  faceId,
+  fingerprint,
+  safe,
+};
