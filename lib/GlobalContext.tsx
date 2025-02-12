ab23f0b6ca7c8aac457cbcacd0a94829a200cc9e
zelan <zelan.xilin@outlook.com>
2025-02-12 19:00:35 +0800

    添加全局上下文提供者和自定义钩子以管理用户登录状态

---
+++ lib/GlobalContext.tsx
@@ -0,0 +1,46 @@
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useState,
+} from "react";
+
+interface GlobalContextType {
+  user?: { account: string };
+  loading: boolean;
+  isLoaded: boolean;
+  login: (account: string, password: string) => void;
+  logout: () => void;
+}
+const GlobalContext = createContext<GlobalContextType | undefined>(undefined);
+
+export const GlobalProvider = ({ children }: { children: ReactNode }) => {
+  const [user, setUser] = useState<GlobalContextType["user"] | undefined>();
+  const [loading, setLoading] = useState(false);
+  const isLoaded = !!user?.account;
+
+  const login = useCallback((account: string, password: string) => {
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+      setUser(account && password ? { account } : undefined);
+    }, 800);
+  }, []);
+  const logout = useCallback(() => {
+    setUser(undefined);
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ user, loading, isLoaded, login, logout }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  const globalContext = useContext(GlobalContext);
+
+  return globalContext;
+};
