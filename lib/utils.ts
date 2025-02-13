53fe64a4001b2ffcbc1ad4d9f4f29873e16f6212
zelan <zelan.xilin@outlook.com>
2025-02-14 00:54:05 +0800

    添加屏幕尺寸和图片大小获取工具函数

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,2 +1,16 @@
+import { Dimensions, Image } from "react-native";
+
 export const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/;
 export const validateEmail = (str: string) => emailRegex.test(str);
+
+export const getScreen = () => {
+  return Dimensions.get("window");
+};
+
+export const getImageSize = (uri: string) => {
+  return new Promise((resolve) => {
+    Image.getSize(uri, (width, height) => {
+      resolve({ width, height, ratio: width / height });
+    });
+  });
+};
